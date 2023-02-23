e0101fc00e0c11cdcb69c73632f6277d6ed6357d
yousseflaarichi <61098645+yousseflaarichi@users.noreply.github.com>
2023-02-23 11:52:56 +0100

    Update testcases.c

--- testcases.c
+++ testcases.c
@@ -1,8 +1,58 @@
+
+#define pair 0
+#define odd 1
+#define unknown 2
 #include<stdio.h>
-int a=1;
-int main()
+int a = 1;
+
+int
+readNumber (int number)
 {
-    printf("hello world %d",a);
-    scanf("%d",&a);
-    return 0;
+  int result = unknown;
+  if (number % 2 == 0)
+    {
+      result = pair;
+
+    }
+  else
+    {
+      result == odd;
+    }
+  return result;
 }
+
+void
+TestReadNumber (int value, int expectedResult)
+{
+  int result = readNumber (value);
+  if (result == expectedResult)
+    {
+      printf ("Passed \n");
+    }
+  else
+    {
+      if (result == unknown)
+	{
+	  printf ("unexpected behavior \n");
+	}
+      else
+	{
+	  printf ("Fails \n");
+	}
+
+    }
+}
+
+int
+main ()
+{
+  TestReadNumber (15, odd);
+  TestReadNumber (257, odd);
+  TestReadNumber (13, odd);
+  TestReadNumber (12, pair);
+  TestReadNumber (28, pair);
+
+  scanf ("%d", &a);
+  return a;
+}
+
